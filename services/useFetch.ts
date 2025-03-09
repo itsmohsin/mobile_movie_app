9523a7e019517fcf48c74d3f9deaf9c19b0652b6
Dji Bangalore <djibang2024@gmail.com>
2025-03-09 14:43:57 +0530

    working on UI

---
+++ services/useFetch.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFuction: () => Promise<T>, autoFetch = true) => {
+    const [data, setData] = useState<T | null>(null);
+    const [error, setError] = useState<Error | null>(null);
+    const[loading, setLoading] = useState(false);
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            setError(null)
+
+            const result = await fetchFuction();
+            setData(result);
+        }catch(err){
+            setError(err instanceof Error ? err : new Error('An error occured'));
+        }finally{
+            setLoading(false);
+        }
+    }
+    const reset=()=>{
+        setData(null);
+        setError(null);
+        setLoading(false);
+    }
+    useEffect(()=>{
+        if(autoFetch) {
+            fetchData();
+        }
+    },[]);
+
+    return { data, error, loading, refetch: fetchData, reset };
+    }
+
+    export default useFetch
